4e25f28421a131526eaeb8e41a8e60138bd328d6
Sergey <xxsmotur@gmail.com>
2020-02-18 15:45:49 +0500

    Handling of logger levels

--- example/example.go
+++ example/example.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
+	"github.com/sirupsen/logrus"
 	"github.com/saromanov/golog"
 )
 func main(){
 	g := golog.New()
-	g.Before(nil)
+	g.Before(func(l *logrus.Logger){
+		l.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp: true,
+		})
+		l.SetOutput(os.Stdout)
+	})
 	g.Infof("test")
 }
--- golog.go
+++ golog.go
@@ -2,13 +2,27 @@ package golog
 
 import (
 	"os"
+
 	"github.com/sirupsen/logrus"
+)
+
+// Level defines logger level
+type Level uint
+
+const (
+	All Level = iota + 1
+	Debug
+	Info
+	Warning
+	Error
+	Fatal
+	Panic
 )
 
 // GoLog implements wrapper over logs
 type GoLog struct {
-	logger    *logrus.Logger
-	showLevel string
+	logger       *logrus.Logger
+	minShowLevel Level
 }
 
 // New creates GoLog
@@ -19,7 +33,7 @@ func New() *GoLog {
 }
 
 // Before implements steps for running before starting of logger
-func (g*GoLog) Before(f func(l *logrus.Logger)){
+func (g *GoLog) Before(f func(l *logrus.Logger)) {
 	if f == nil {
 		g.beforeDefault()
 		return
@@ -27,7 +41,7 @@ func (g*GoLog) Before(f func(l *logrus.Logger)){
 	f(g.logger)
 }
 
-func (g *GoLog) beforeDefault(){
+func (g *GoLog) beforeDefault() {
 	g.logger.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
@@ -35,16 +49,41 @@ func (g *GoLog) beforeDefault(){
 }
 
 // Fatalf for fatal errors
-func (l *GoLog) Fatalf(format string, data ...interface{}) {
-	l.logger.Fatalf(format, data...)
+func (g *GoLog) Fatalf(format string, data ...interface{}) {
+	if g.minShowLevel > Fatal {
+		return
+	}
+	g.logger.Fatalf(format, data...)
 }
 
 // Infof for info errors
-func (l *GoLog) Infof(format string, data ...interface{}) {
-	l.logger.Infof(format, data...)
+func (g *GoLog) Infof(format string, data ...interface{}) {
+	if g.minShowLevel > Info {
+		return
+	}
+	g.logger.Infof(format, data...)
 }
 
 // Errorf for errors with "Error" level
-func (l *GoLog) Errorf(format string, data ...interface{}) {
-	l.logger.Errorf(format, data...)
+func (g *GoLog) Errorf(format string, data ...interface{}) {
+	if g.minShowLevel > Error {
+		return
+	}
+	g.logger.Errorf(format, data...)
+}
+
+// Panicf for panic errors
+func (g *GoLog) Panicf(format string, data ...interface{}) {
+	if g.minShowLevel > Panic {
+		return
+	}
+	g.logger.Panicf(format, data...)
+}
+
+// Warningf for warning errors
+func (g *GoLog) Warningf(format string, data ...interface{}) {
+	if g.minShowLevel > Warning {
+		return
+	}
+	g.logger.Warningf(format, data...)
 }
